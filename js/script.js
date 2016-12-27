a98528ab947b577432662fe0dc6eb0628298014b
Miyuyami <tenshi.tenshi.angel@gmail.com>
2016-12-27 10:10:57 -0800

    language loading changed
    no longer uses display block/none to display a different language
    now languages are loaded dynamically by jquery .load() from another
    .html

--- js/script.js
+++ js/script.js
@@ -1,8 +1,4 @@
 $(document).ready(function() {
-	$("a.link_setup").attr("href", "https://raw.githubusercontent.com/Miyuyami/SWPatcher/master/SWPatcher/publish/setup.exe");
-	$("a.link_vt_setup").attr("href", "https://www.virustotal.com/en/file/b8d746df3db48871c296cdb91e342f50e615ec22dcec8fe0d537efb92dfabdd1/analysis/1474016329/");
-	$("a.link_vt_exe").attr("href", "https://www.virustotal.com/en/file/afac231afc668a8f5089ad78c2c6a1def4c8713803c2cedee25ebede8e694038/analysis/1476569778/");
-	$("iframe.link_yt_tutorial").attr("src", "https://www.youtube.com/embed/wogDNmHRzbw?rel=0");
 	var cookieValue = Cookies.get("language");
 	var $lang_bar = $(".navbar-right");
 	var $lang_dropdown = $lang_bar.children(".dropdown");
@@ -46,13 +42,11 @@ $(".navbar-right").on("click", ".dropdown li.clickable", function (e) {
 	$menu_children.addClass("clickable");
 	$menu_children.removeClass("active");
 	if (lang_code == "ko") {
-		$("div:lang(ko)").css("display", "block");
-		$("div:lang(en)").css("display", "none");
+		$("#lang_content").load("patcher.kr.html");
 		Cookies.set("language", "ko", { expires: 20*365});
 	}
 	else { //if (lang_code == "en") {
-		$("div:lang(ko)").css("display", "none");
-		$("div:lang(en)").css("display", "block");
+		$("#lang_content").load("patcher.en.html");
 		Cookies.set("language", "en", { expires: 20*365});
 	}
 });
