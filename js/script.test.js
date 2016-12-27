8beb11d34caf950cce20232a323407cda577cba1
Miyuyami <tenshi.tenshi.angel@gmail.com>
2016-12-27 10:03:45 -0800

    the test

--- js/script.test.js
+++ js/script.test.js
@@ -1,8 +1,4 @@
 $(document).ready(function() {
-	$("a.link_setup").attr("href", "https://raw.githubusercontent.com/Miyuyami/SWPatcher/master/SWPatcher/publish/setup.exe");
-	$("a.link_vt_setup").attr("href", "https://www.virustotal.com/en/file/b8d746df3db48871c296cdb91e342f50e615ec22dcec8fe0d537efb92dfabdd1/analysis/1474016329/");
-	$("a.link_vt_exe").attr("href", "https://www.virustotal.com/en/file/afac231afc668a8f5089ad78c2c6a1def4c8713803c2cedee25ebede8e694038/analysis/1476569778/");
-	$("iframe.link_yt_tutorial").attr("src", "https://www.youtube.com/embed/wogDNmHRzbw?rel=0");
 	var cookieValue = Cookies.get("language");
 	var $lang_bar = $(".navbar-right");
 	var $lang_dropdown = $lang_bar.children(".dropdown");
