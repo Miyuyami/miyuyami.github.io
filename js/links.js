8d841dd36d7576dbb13a9a51e3c1184b8330c2c1
Miyuyami <tenshi.tenshi.angel@gmail.com>
2017-01-03 09:58:54 -0800

    updated changelog and VT link

--- js/links.js
+++ js/links.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	$("a.link_setup").attr("href", "https://raw.githubusercontent.com/Miyuyami/SWPatcher/master/SWPatcher/publish/setup.exe");
 	$("a.link_vt_setup").attr("href", "https://www.virustotal.com/en/file/748b8f082d3ac01efd4dee26333985c297a258834cb6ecf94b3613b5242a6709/analysis/1482964401/");
-	$("a.link_vt_exe").attr("href", "https://www.virustotal.com/en/file/48bc1f8ed91618d6da4e9943a68e61021c894e3c80b721d2b9a465e6cad666a7/analysis/1483450611/");
+	$("a.link_vt_exe").attr("href", "https://www.virustotal.com/en/file/13db3d0ec8a78a3772046cec2803e24b74a0fb7007643e04778ded7d79a22508/analysis/1483465960/");
 	$("iframe.link_yt_tutorial").attr("src", "https://www.youtube.com/embed/wogDNmHRzbw?rel=0");
 });
