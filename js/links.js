a69f84f92f6bf6a82074fed7745e5443721d9036
Miyuyami <tenshi.tenshi.angel@gmail.com>
2017-01-08 05:40:26 -0800

    typo in Common Problems
    removing unused js files

--- js/links.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-	$("a.link_setup").attr("href", "https://raw.githubusercontent.com/Miyuyami/SWPatcher/master/SWPatcher/publish/setup.exe");
-	$("a.link_vt_setup").attr("href", "https://www.virustotal.com/en/file/748b8f082d3ac01efd4dee26333985c297a258834cb6ecf94b3613b5242a6709/analysis/1482964401/");
-	$("a.link_vt_exe").attr("href", "https://www.virustotal.com/en/file/13db3d0ec8a78a3772046cec2803e24b74a0fb7007643e04778ded7d79a22508/analysis/1483465960/");
-	$("iframe.link_yt_tutorial").attr("src", "https://www.youtube.com/embed/wogDNmHRzbw?rel=0");
-});
