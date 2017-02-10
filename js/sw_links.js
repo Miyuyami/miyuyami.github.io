275c0617ea3282345b6e2fa264495076fc401083
Miyuyami <tenshi.tenshi.angel@gmail.com>
2017-02-10 09:22:58 -0800

    updated changelog and vt link to 2.7.1.0

--- js/sw_links.js
+++ js/sw_links.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	$("a.link_setup").attr("href", "https://raw.githubusercontent.com/Miyuyami/SWPatcher/master/SWPatcher/publish/setup.exe");
 	$("a.link_vt_setup").attr("href", "https://www.virustotal.com/en/file/748b8f082d3ac01efd4dee26333985c297a258834cb6ecf94b3613b5242a6709/analysis/1482964401/");
-	$("a.link_vt_exe").attr("href", "https://www.virustotal.com/en/file/3e77c48795b5f995a4fef1267d59ec8e5c82ae5164cd8a75f4ef3379df0ab7fd/analysis/1486482793/");
+	$("a.link_vt_exe").attr("href", "https://www.virustotal.com/en/file/6cdd49da95f648497cfa93e1f8f42485004f413a65d5baa0b6f43f20a3d89d4c/analysis/1486747325/");
 	$("iframe.link_yt_tutorial").attr("src", "https://www.youtube.com/embed/wogDNmHRzbw?rel=0");
 });
