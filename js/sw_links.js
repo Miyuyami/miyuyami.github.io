ca2adc96374e8f15480f6e9b2f0647d9032f712f
Miyuyami <tenshi.tenshi.angel@gmail.com>
2017-02-26 07:42:09 -0800

    updated changelog and vt link to 2.8.1.0

--- js/sw_links.js
+++ js/sw_links.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	$("a.link_setup").attr("href", "https://raw.githubusercontent.com/Miyuyami/SWPatcher/master/SWPatcher/publish/setup.exe");
 	$("a.link_vt_setup").attr("href", "https://www.virustotal.com/en/file/748b8f082d3ac01efd4dee26333985c297a258834cb6ecf94b3613b5242a6709/analysis/1482964401/");
-	$("a.link_vt_exe").attr("href", "https://www.virustotal.com/en/file/a7697bc3a5332e385b3ca988b45a14e8fcbb8496ca3bcf48d31840d3d39ebaed/analysis/1488084322/");
+	$("a.link_vt_exe").attr("href", "https://www.virustotal.com/en/file/43420b290b3a6e94271ef7a4f2b8550e2303fb3ebd538b4fdfe6c20f8c0c55fc/analysis/1488123683/");
 	$("iframe.link_yt_tutorial").attr("src", "https://www.youtube.com/embed/wogDNmHRzbw?rel=0");
 });
