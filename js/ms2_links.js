743925e6f3e2ff5ed4e70f47b2b371613df56c47
Miyuyami <tenshi.tenshi.angel@gmail.com>
2017-12-13 20:00:04 +0100

    added ms2 patches short page

---
+++ js/ms2_links.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+	$("a.link_cn_en").attr("href", "https://www.dropbox.com/s/8j3o81ifxu3gu3k/Xml%20English.zip?dl=1");
+	$("a.link_cn_ko").attr("href", "https://www.dropbox.com/s/g7nax01y4yjlonf/Xml%20Korean.zip?dl=1");
+});
