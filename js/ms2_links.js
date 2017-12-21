5e686d9f59e64fb8e7ede399b7fc1209457eb4d7
Miyuyami <tenshi.tenshi.angel@gmail.com>
2017-12-21 18:03:05 +0100

    added patches for the korean maplestory2

--- js/ms2_links.js
+++ js/ms2_links.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
 	$("a.link_cn_en").attr("href", "https://www.dropbox.com/s/8j3o81ifxu3gu3k/Xml%20English.zip?dl=1");
 	$("a.link_cn_ko").attr("href", "https://www.dropbox.com/s/g7nax01y4yjlonf/Xml%20Korean.zip?dl=1");
+	$("a.link_kr_en").attr("href", "https://www.dropbox.com/s/lw0mmoewokp8shi/Xml%20English.zip?dl=1");
+	$("a.link_kr_zh").attr("href", "https://www.dropbox.com/s/lxrdi6k1qo2zx49/Xml%20Chinese.zip?dl=1");
 });
