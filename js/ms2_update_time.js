2588a6577a84b546271b12ae551c82ad64cc7d0d
Miyuyami <tenshi.tenshi.angel@gmail.com>
2018-04-27 16:33:10 +0200

    added a way to see the time the patch was last updated

---
+++ js/ms2_update_time.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	$.getJSON("https://cors.io/?https://www.dropbox.com/s/25xodqbkn5r8dyz/updated.json?dl=1", function(data) {
+		var fields = Object.keys(data);
+		for (var i in fields) {
+			var date = new Date(data[fields[i]]);
+			$("span." + fields[i]).text(moment(data[fields[i]]).fromNow());
+		}
+	});
+});
