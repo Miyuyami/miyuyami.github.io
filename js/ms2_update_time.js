3cffc148d35014d5302b12ddade253d6d4309f2d
Miyuyami <tenshi.tenshi.angel@gmail.com>
2018-04-27 19:58:44 +0200

    fixed momentjs format to properly work on different local timezones

--- js/ms2_update_time.js
+++ js/ms2_update_time.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
 	$.getJSON("https://cors.io/?https://www.dropbox.com/s/25xodqbkn5r8dyz/updated.json?dl=1", function(data) {
 		var fields = Object.keys(data);
 		for (var i in fields) {
-			var date = new Date(data[fields[i]]);
-			$("span." + fields[i]).text(moment(data[fields[i]], "DD/MM/YYYY h:mm A UTC+Z").fromNow());
+			var m = moment(data[fields[i]], "DD/MM/YYYY hh:mm A ZZ");
+			$("span." + fields[i]).text(m.fromNow());
 		}
 	});
 });
