ef1dd25da6afa6d7e1855537bd555a2f80451e7b
Miyuyami <tenshi.tenshi.angel@gmail.com>
2018-08-04 19:21:17 +0200

    fix some ms2 links

--- js/ms2_links.js
+++ js/ms2_links.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
-	$("a.link_cn_en").attr("href", "https://www.dropbox.com/s/8j3o81ifxu3gu3k/Xml%20English.zip?dl=1");
+	$("a.link_cn_en").attr("href", "https://www.dropbox.com/s/d9ojnxja5sra867/Xml%20English.zip?dl=1");
 	$("a.link_cn_ko").attr("href", "https://www.dropbox.com/s/g7nax01y4yjlonf/Xml%20Korean.zip?dl=1");
 	$("a.link_cn_en_noname").attr("href", "https://www.dropbox.com/s/ws7wgg7lgnmj4vr/Xml%20English%20%28No%20Names%20ver%29.zip?dl=1");
 	$("a.link_cn_ko_noname").attr("href", "https://www.dropbox.com/s/dqunzarctxzn1lx/Xml%20Korean%20%28No%20Names%20ver%29.zip?dl=1");
 	
-	$("a.link_kr_en").attr("href", "https://www.dropbox.com/s/lw0mmoewokp8shi/Xml%20English.zip?dl=1");
+	$("a.link_kr_en").attr("href", "https://www.dropbox.com/s/8ja2hwufh2kxrs6/Xml%20English.zip?dl=1");
 	$("a.link_kr_zh").attr("href", "https://www.dropbox.com/s/lxrdi6k1qo2zx49/Xml%20Chinese.zip?dl=1");
 	$("a.link_kr_en_noname").attr("href", "https://www.dropbox.com/s/wkzrdibqyx5e2xo/Xml%20English%20%28No%20Names%20ver%29.zip?dl=1");
 	$("a.link_kr_zh_noname").attr("href", "https://www.dropbox.com/s/4pqv832u18gu27o/Xml%20Chinese%20%28No%20Names%20ver%29.zip?dl=1");
