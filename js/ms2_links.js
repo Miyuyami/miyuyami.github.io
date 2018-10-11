80d2f1fa4960b9757c93d3c85c8f28879fee88f1
Miyuyami <tenshi.tenshi.angel@gmail.com>
2018-10-11 18:24:49 +0200

    [MapleStory 2] added global patches

--- js/ms2_links.js
+++ js/ms2_links.js
@@ -8,4 +8,7 @@ $(document).ready(function() {
 	$("a.link_kr_zh").attr("href", "https://www.dropbox.com/s/lxrdi6k1qo2zx49/Xml%20Chinese.zip?dl=1");
 	$("a.link_kr_en_noname").attr("href", "https://www.dropbox.com/s/wkzrdibqyx5e2xo/Xml%20English%20%28No%20Names%20ver%29.zip?dl=1");
 	$("a.link_kr_zh_noname").attr("href", "https://www.dropbox.com/s/4pqv832u18gu27o/Xml%20Chinese%20%28No%20Names%20ver%29.zip?dl=1");
+	
+	$("a.link_gb").attr("href", "https://www.dropbox.com/s/scepv8sk3dd27er/Xml.zip?dl=1");
+	$("a.link_gb_noname").attr("href", "https://www.dropbox.com/s/o9j7eexnjk8bawv/Xml%20%28No%20Names%20ver%29.zip?dl=1");
 });
