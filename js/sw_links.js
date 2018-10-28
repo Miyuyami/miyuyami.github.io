08899e6a89491be83110bd9f5ef29bede8979962
Miyuyami <tenshi.tenshi.angel@gmail.com>
2018-10-28 09:49:22 +0100

    [Soulworker Patcher] updated changelog and vt links to 2.9.0.1

--- js/sw_links.js
+++ js/sw_links.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	$("a.link_setup").attr("href", "https://raw.githubusercontent.com/Miyuyami/SWPatcher/master/SWPatcher/publish/setup.exe");
 	$("a.link_vt_setup").attr("href", "https://www.virustotal.com/#/file/640b60c626cf7e98e8e222a6cc91db177603ffb51a616235dbcc9fd1c0861b92/detection");
-	$("a.link_vt_exe").attr("href", "https://www.virustotal.com/#/file/e37c4d5aeb66464f9924ab8a4404d1b7d6ac6d423b91988fa09c8873dbd5be74/detection");
+	$("a.link_vt_exe").attr("href", "https://www.virustotal.com/#/file/ca85eebaccaca37f909ab021df5e15792bfbcfc9b956a2208a0d33e2b411cd5f/detection");
 	$("iframe.link_yt_tutorial").attr("src", "https://www.youtube.com/embed/wogDNmHRzbw?rel=0");
 });
