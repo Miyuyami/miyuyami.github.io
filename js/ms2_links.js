b239dc5a39cf4030b931f1103750876302d1c843
Miyuyami <tenshi.tenshi.angel@gmail.com>
2018-10-31 13:42:16 +0100

    [MapleStory 2] added google drive links for patches

--- js/ms2_links.js
+++ js/ms2_links.js
@@ -13,5 +13,21 @@ $(document).ready(function() {
 	$("a.link_dropbox_gb_noname").attr("href", "https://dl.dropboxusercontent.com/s/o9j7eexnjk8bawv/Xml%20%28No%20Names%20ver%29.zip");
 	$("a.link_dropbox_gb_fontsonly").attr("href", "https://dl.dropboxusercontent.com/s/ykj7xqsv907didw/Xml%20%28Fonts%20only%29.zip");
 	
+	
+	$("a.link_gdrive_cn_en").attr("href", "https://drive.google.com/uc?export=download&id=1WNUB4JBrFGp63r-gh_HTCQAMFEdvD_Dq");
+	$("a.link_gdrive_cn_ko").attr("href", "https://drive.google.com/uc?export=download&id=1vWY7vJ4W7zd2pDwmcZLBwEqUkbV1o1Vr");
+	$("a.link_gdrive_cn_en_noname").attr("href", "https://drive.google.com/uc?export=download&id=13A2vt7T_QGoeXy7A_orbrntBWFiwyzS7");
+	$("a.link_gdrive_cn_ko_noname").attr("href", "https://drive.google.com/uc?export=download&id=1wzL4mO9xfGEJkxxJ5s4mxFcqWjZzt28W");
+	
+	$("a.link_gdrive_kr_en").attr("href", "https://drive.google.com/uc?export=download&id=1QsGWztJUSnlsSX1C3QikMLo9Fh5UGZxp");
+	$("a.link_gdrive_kr_zh").attr("href", "https://drive.google.com/uc?export=download&id=1Itr3Ero3VFf0OuuqMY9ThiivX8vgGX3Q");
+	$("a.link_gdrive_kr_en_noname").attr("href", "https://drive.google.com/uc?export=download&id=1MuEzLLG9A530n1-BNdiGhW5GQiA3tooC");
+	$("a.link_gdrive_kr_zh_noname").attr("href", "https://drive.google.com/uc?export=download&id=1v-DXaKjrk65d-o0zmbKv3hXzEl52vbwX");
+	
+	$("a.link_gdrive_gb").attr("href", "https://drive.google.com/uc?export=download&id=1tw1_tzCViJkiXsrRepdwBuYK3g-KMJwF");
+	$("a.link_gdrive_gb_noname").attr("href", "https://drive.google.com/uc?export=download&id=1yfXf--tfYAzvth9OWhCuEj3yAVTepMNt");
+	$("a.link_gdrive_gb_fontsonly").attr("href", "https://drive.google.com/uc?export=download&id=1yNuTQSSSpnyU6h_udILEAwQuWFkXLNO6");
+	
+	
 	$("a.link_mega_sound_kr").attr("href", "https://mega.nz/#!q14DRS7J!IeHl4c4bpEjwtXVuNUawOMmnzQLakQXYa_JB6H2NguA");
 });
