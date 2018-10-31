949fdb5dd9fdf3414db85174686c28b3f5180d77
Miyuyami <tenshi.tenshi.angel@gmail.com>
2018-10-31 13:22:59 +0100

    [MapleStory 2] changed link ids to contain their upload location

--- js/ms2_links.js
+++ js/ms2_links.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
-	$("a.link_cn_en").attr("href", "https://dl.dropboxusercontent.com/s/d9ojnxja5sra867/Xml%20English.zip");
-	$("a.link_cn_ko").attr("href", "https://dl.dropboxusercontent.com/s/g7nax01y4yjlonf/Xml%20Korean.zip");
-	$("a.link_cn_en_noname").attr("href", "https://dl.dropboxusercontent.com/s/ws7wgg7lgnmj4vr/Xml%20English%20%28No%20Names%20ver%29.zip");
-	$("a.link_cn_ko_noname").attr("href", "https://dl.dropboxusercontent.com/s/dqunzarctxzn1lx/Xml%20Korean%20%28No%20Names%20ver%29.zip");
+	$("a.link_dropbox_cn_en").attr("href", "https://dl.dropboxusercontent.com/s/d9ojnxja5sra867/Xml%20English.zip");
+	$("a.link_dropbox_cn_ko").attr("href", "https://dl.dropboxusercontent.com/s/g7nax01y4yjlonf/Xml%20Korean.zip");
+	$("a.link_dropbox_cn_en_noname").attr("href", "https://dl.dropboxusercontent.com/s/ws7wgg7lgnmj4vr/Xml%20English%20%28No%20Names%20ver%29.zip");
+	$("a.link_dropbox_cn_ko_noname").attr("href", "https://dl.dropboxusercontent.com/s/dqunzarctxzn1lx/Xml%20Korean%20%28No%20Names%20ver%29.zip");
 	
-	$("a.link_kr_en").attr("href", "https://dl.dropboxusercontent.com/s/8ja2hwufh2kxrs6/Xml%20English.zip");
-	$("a.link_kr_zh").attr("href", "https://dl.dropboxusercontent.com/s/lxrdi6k1qo2zx49/Xml%20Chinese.zip");
-	$("a.link_kr_en_noname").attr("href", "https://dl.dropboxusercontent.com/s/wkzrdibqyx5e2xo/Xml%20English%20%28No%20Names%20ver%29.zip");
-	$("a.link_kr_zh_noname").attr("href", "https://dl.dropboxusercontent.com/s/4pqv832u18gu27o/Xml%20Chinese%20%28No%20Names%20ver%29.zip");
+	$("a.link_dropbox_kr_en").attr("href", "https://dl.dropboxusercontent.com/s/8ja2hwufh2kxrs6/Xml%20English.zip");
+	$("a.link_dropbox_kr_zh").attr("href", "https://dl.dropboxusercontent.com/s/lxrdi6k1qo2zx49/Xml%20Chinese.zip");
+	$("a.link_dropbox_kr_en_noname").attr("href", "https://dl.dropboxusercontent.com/s/wkzrdibqyx5e2xo/Xml%20English%20%28No%20Names%20ver%29.zip");
+	$("a.link_dropbox_kr_zh_noname").attr("href", "https://dl.dropboxusercontent.com/s/4pqv832u18gu27o/Xml%20Chinese%20%28No%20Names%20ver%29.zip");
 	
-	$("a.link_gb").attr("href", "https://dl.dropboxusercontent.com/s/scepv8sk3dd27er/Xml.zip");
-	$("a.link_gb_noname").attr("href", "https://dl.dropboxusercontent.com/s/o9j7eexnjk8bawv/Xml%20%28No%20Names%20ver%29.zip");
-	$("a.link_gb_fontsonly").attr("href", "https://dl.dropboxusercontent.com/s/ykj7xqsv907didw/Xml%20%28Fonts%20only%29.zip");
+	$("a.link_dropbox_gb").attr("href", "https://dl.dropboxusercontent.com/s/scepv8sk3dd27er/Xml.zip");
+	$("a.link_dropbox_gb_noname").attr("href", "https://dl.dropboxusercontent.com/s/o9j7eexnjk8bawv/Xml%20%28No%20Names%20ver%29.zip");
+	$("a.link_dropbox_gb_fontsonly").attr("href", "https://dl.dropboxusercontent.com/s/ykj7xqsv907didw/Xml%20%28Fonts%20only%29.zip");
 	
-	$("a.link_sound_kr").attr("href", "https://mega.nz/#!q14DRS7J!IeHl4c4bpEjwtXVuNUawOMmnzQLakQXYa_JB6H2NguA");
+	$("a.link_mega_sound_kr").attr("href", "https://mega.nz/#!q14DRS7J!IeHl4c4bpEjwtXVuNUawOMmnzQLakQXYa_JB6H2NguA");
 });
