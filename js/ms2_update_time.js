781cd95bcd2dfec432b1f598d8950cd77fc73145
Miyuyami <tenshi.tenshi.angel@gmail.com>
2018-10-31 13:20:22 +0100

    [MapleStory 2] use direct dropbox links for patches

--- js/ms2_update_time.js
+++ js/ms2_update_time.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	$.getJSON("https://cors.io/?https://www.dropbox.com/s/25xodqbkn5r8dyz/updated.json?dl=1", function(data) {
+	$.getJSON("https://dl.dropboxusercontent.com/s/25xodqbkn5r8dyz/updated.json", function(data) {
 		var fields = Object.keys(data);
 		for (var i in fields) {
 			var m = moment(data[fields[i]], "DD/MM/YYYY hh:mm A ZZ");
