74d7fb8d94045a844aba619c54f6e601fe41c5d6
Miyuyami <tenshi.tenshi.angel@gmail.com>
2018-10-31 14:10:14 +0100

    [MapleStory 2] changed all patches links to google drive

--- js/ms2_update_time.js
+++ js/ms2_update_time.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	$.getJSON("https://dl.dropboxusercontent.com/s/25xodqbkn5r8dyz/updated.json", function(data) {
+	$.getJSON("https://cors.io/?https://drive.google.com/uc?export=download&id=1nhiBp_nKcJ5PfTIDr0CIQjoauJ4-1kRD", function(data) {
 		var fields = Object.keys(data);
 		for (var i in fields) {
 			var m = moment(data[fields[i]], "DD/MM/YYYY hh:mm A ZZ");
