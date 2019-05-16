f1ded27fbcafa0e6ee10d2139813ad0d1158c1f7
Miyuyami <tenshi.tenshi.angel@gmail.com>
2019-05-16 13:30:39 +0300

    [MapleStory 2] added japanese patches

--- ms2/js/links.js
+++ ms2/js/links.js
@@ -1,13 +1,24 @@
 $(document).ready(() => {
 	$("#link_dropbox_cn_en").attr("href", "https://dl.dropboxusercontent.com/s/d9ojnxja5sra867/Xml%20English.zip");
 	$("#link_dropbox_cn_ko").attr("href", "https://dl.dropboxusercontent.com/s/g7nax01y4yjlonf/Xml%20Korean.zip");
+	$("#link_dropbox_cn_ja").attr("href", "ttps://dl.dropboxusercontent.com/s/qyi2fegsx0umjh3/Xml%20Japanese.zip");
 	$("#link_dropbox_cn_en_noname").attr("href", "https://dl.dropboxusercontent.com/s/ws7wgg7lgnmj4vr/Xml%20English%20%28No%20Names%20ver%29.zip");
 	$("#link_dropbox_cn_ko_noname").attr("href", "https://dl.dropboxusercontent.com/s/dqunzarctxzn1lx/Xml%20Korean%20%28No%20Names%20ver%29.zip");
+	$("#link_dropbox_cn_ja_noname").attr("href", "ttps://dl.dropboxusercontent.com/s/v1idr6rou0plldo/Xml%20Japanese%20%28No%20Names%20ver%29.zip");
 	
 	$("#link_dropbox_kr_en").attr("href", "https://dl.dropboxusercontent.com/s/8ja2hwufh2kxrs6/Xml%20English.zip");
 	$("#link_dropbox_kr_zh").attr("href", "https://dl.dropboxusercontent.com/s/lxrdi6k1qo2zx49/Xml%20Chinese.zip");
+	$("#link_dropbox_kr_ja").attr("href", "https://dl.dropboxusercontent.com/s/7jgi8dj6p6ri0pg/Xml%20Japanese.zip");
 	$("#link_dropbox_kr_en_noname").attr("href", "https://dl.dropboxusercontent.com/s/wkzrdibqyx5e2xo/Xml%20English%20%28No%20Names%20ver%29.zip");
 	$("#link_dropbox_kr_zh_noname").attr("href", "https://dl.dropboxusercontent.com/s/4pqv832u18gu27o/Xml%20Chinese%20%28No%20Names%20ver%29.zip");
+	$("#link_dropbox_kr_ja_noname").attr("href", "https://dl.dropboxusercontent.com/s/i32o5e4e1hp3rx1/Xml%20Japanese%20%28No%20Names%20ver%29.zip");
+	
+	$("#link_dropbox_jp_en").attr("href", "https://dl.dropboxusercontent.com/s/h8h2x403x7fghtr/Xml%20English.zip");
+	$("#link_dropbox_jp_zh").attr("href", "https://dl.dropboxusercontent.com/s/yybre0732tgnth4/Xml%20Chinese.zip");
+	$("#link_dropbox_jp_ko").attr("href", "https://dl.dropboxusercontent.com/s/oi8m5sl1s3wn5wl/Xml%20Korean.zip");
+	$("#link_dropbox_jp_en_noname").attr("href", "https://dl.dropboxusercontent.com/s/tusqo9rj6csilzo/Xml%20English%20%28No%20Names%20ver%29.zip");
+	$("#link_dropbox_jp_zh_noname").attr("href", "https://dl.dropboxusercontent.com/s/fciuik7wweqehm9/Xml%20Chinese%20%28No%20Names%20ver%29.zip");
+	$("#link_dropbox_jp_ko_noname").attr("href", "https://dl.dropboxusercontent.com/s/qciv3bmamex5blr/Xml%20Korean%20%28No%20Names%20ver%29.zip");
 	
 	$("#link_dropbox_gb").attr("href", "https://dl.dropboxusercontent.com/s/scepv8sk3dd27er/Xml.zip");
 	$("#link_dropbox_gb_noname").attr("href", "https://dl.dropboxusercontent.com/s/o9j7eexnjk8bawv/Xml%20%28No%20Names%20ver%29.zip");
@@ -16,21 +27,33 @@ $(document).ready(() => {
 	
 	$("#link_gdrive_cn_en").attr("href", "https://drive.google.com/uc?export=download&id=1WNUB4JBrFGp63r-gh_HTCQAMFEdvD_Dq");
 	$("#link_gdrive_cn_ko").attr("href", "https://drive.google.com/uc?export=download&id=1vWY7vJ4W7zd2pDwmcZLBwEqUkbV1o1Vr");
+	$("#link_gdrive_cn_ja").attr("href", "https://drive.google.com/uc?export=download&id=1JI73xKGLcc60FvdSMLmIvbg5QsKTe5rn");
 	$("#link_gdrive_cn_en_noname").attr("href", "https://drive.google.com/uc?export=download&id=13A2vt7T_QGoeXy7A_orbrntBWFiwyzS7");
 	$("#link_gdrive_cn_ko_noname").attr("href", "https://drive.google.com/uc?export=download&id=1wzL4mO9xfGEJkxxJ5s4mxFcqWjZzt28W");
+	$("#link_gdrive_cn_ja_noname").attr("href", "https://drive.google.com/uc?export=download&id=1LbHwWfjU5O8rIO5sGEyoWtRST__Pq5Hl");
 	
 	$("#link_gdrive_kr_en").attr("href", "https://drive.google.com/uc?export=download&id=1QsGWztJUSnlsSX1C3QikMLo9Fh5UGZxp");
 	$("#link_gdrive_kr_zh").attr("href", "https://drive.google.com/uc?export=download&id=1Itr3Ero3VFf0OuuqMY9ThiivX8vgGX3Q");
+	$("#link_gdrive_kr_ja").attr("href", "https://drive.google.com/uc?export=download&id=153ORb5gOBeOpEZLtrbz_MkseohUwA3Xg");
 	$("#link_gdrive_kr_en_noname").attr("href", "https://drive.google.com/uc?export=download&id=1MuEzLLG9A530n1-BNdiGhW5GQiA3tooC");
 	$("#link_gdrive_kr_zh_noname").attr("href", "https://drive.google.com/uc?export=download&id=1v-DXaKjrk65d-o0zmbKv3hXzEl52vbwX");
+	$("#link_gdrive_kr_ja_noname").attr("href", "https://drive.google.com/uc?export=download&id=15p_fStNZgTfIdfwTuTXSmhvwTznTQhFn");
+	
+	$("#link_gdrive_jp_en").attr("href", "https://drive.google.com/uc?export=download&id=1htSMHJbi8Qks4prOpBpJDz6v--8nh09G");
+	$("#link_gdrive_jp_zh").attr("href", "https://drive.google.com/uc?export=download&id=17dUPKJoSxXzbCB_3JQWo7_kLg-mqbiyN");
+	$("#link_gdrive_jp_ko").attr("href", "https://drive.google.com/uc?export=download&id=1H4w9b0SIZGqr_edrczmrccz7Ye3YWuVh");
+	$("#link_gdrive_jp_en_noname").attr("href", "https://drive.google.com/uc?export=download&id=1Ur7VhUCdjOglxty7LO7716spy94b88MS");
+	$("#link_gdrive_jp_zh_noname").attr("href", "https://drive.google.com/uc?export=download&id=1EgbAELQkEtaQu1VrIdyiAF9PI2LQaGEa");
+	$("#link_gdrive_jp_ko_noname").attr("href", "https://drive.google.com/uc?export=download&id=1yiSctlzM-edTk9c57wp62e9j7YHKP0k-");
 	
 	$("#link_gdrive_gb").attr("href", "https://drive.google.com/uc?export=download&id=1tw1_tzCViJkiXsrRepdwBuYK3g-KMJwF");
 	$("#link_gdrive_gb_noname").attr("href", "https://drive.google.com/uc?export=download&id=1yfXf--tfYAzvth9OWhCuEj3yAVTepMNt");
 	$("#link_gdrive_gb_fontsonly").attr("href", "https://drive.google.com/uc?export=download&id=1yNuTQSSSpnyU6h_udILEAwQuWFkXLNO6");
 	
 	
-	$("#link_mega_sound_kr").attr("href", "https://mega.nz/#!q14DRS7J!IeHl4c4bpEjwtXVuNUawOMmnzQLakQXYa_JB6H2NguA");
-	$("#link_mega_sound_cn").attr("href", "https://mega.nz/#!7so3mSzR!n3H8iFRfNhsgVG_SkcYIC4_Kwk7Y2lt8IJeGSF9np_8");
+	$("#link_mega_sound_kr").attr("href", "https://mega.nz/#!KgBgTCaB!6pRJPi0LCAf5VfbJpSdlfHrdijIzHUplEGTb8g0APHQ");
+	$("#link_mega_sound_cn").attr("href", "https://mega.nz/#!bwBGhYzA!-HX-mMe2m4SpqTUsb49qt_L8FEleciPvJxrSXfjAU10");
+	$("#link_mega_sound_jp").attr("href", "https://mega.nz/#!HwAiUajY!zzCqz47I5rdVcIP0usEaWVhJkiXyDun8KGt_a2v5DIw");
 	
 	
 	$("#link_github_ms2tools_releases").attr("href", "https://github.com/Miyuyami/MS2Tools/releases");
