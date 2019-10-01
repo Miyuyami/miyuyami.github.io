8ada402a12dc1cd08230a39bd4c97c1ebaf4a2d7
Miyuyami <tenshi.tenshi.angel@gmail.com>
2019-10-01 17:19:56 +0300

    [Soulworker] updated VT exe link (2.12.0.0)

--- sw/js/links.js
+++ sw/js/links.js
@@ -1,6 +1,6 @@
 $(document).ready(() => {
 	$("#link_setup").attr("href", "https://raw.githubusercontent.com/Miyuyami/SWPatcher/master/SWPatcher/publish/setup.exe");
 	$("#link_vt_setup").attr("href", "https://www.virustotal.com/#/file/640b60c626cf7e98e8e222a6cc91db177603ffb51a616235dbcc9fd1c0861b92/detection");
-	$("#link_vt_exe").attr("href", "https://www.virustotal.com/#/file/ca85eebaccaca37f909ab021df5e15792bfbcfc9b956a2208a0d33e2b411cd5f/detection");
+	$("#link_vt_exe").attr("href", "https://www.virustotal.com/gui/file-analysis/NDVmZTBhZTUzOTc1MTM5NzdlOTJjY2YwYTNhYjZhMzE6MTU2OTkzOTUxOQ==/detection");
 	$("#link_yt_tutorial").attr("src", "https://www.youtube.com/embed/wogDNmHRzbw?rel=0");
 });
