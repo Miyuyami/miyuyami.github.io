74380bf08831ad1a319417b4e09ec9d63b2039f2
Miyuyami <tenshi.tenshi.angel@gmail.com>
2019-10-16 15:04:50 +0300

    [Soulworker] updated patcher to 2.12.3.0

--- sw/js/links.js
+++ sw/js/links.js
@@ -1,6 +1,6 @@
 $(document).ready(() => {
 	$("#link_setup").attr("href", "https://raw.githubusercontent.com/Miyuyami/SWPatcher/master/SWPatcher/publish/setup.exe");
 	$("#link_vt_setup").attr("href", "https://www.virustotal.com/#/file/640b60c626cf7e98e8e222a6cc91db177603ffb51a616235dbcc9fd1c0861b92/detection");
-	$("#link_vt_exe").attr("href", "https://www.virustotal.com/gui/file-analysis/YzVkMDUwZTViNDY2MTJlYWY4ZmZjMzMwZDhhOTFhOGY6MTU2OTk0ODIyOQ==/detection");
+	$("#link_vt_exe").attr("href", "https://www.virustotal.com/gui/file-analysis/NGMxNjBkNTllNjBhZjQxMjZmOTk0ZmE2MzZjZGY3YzY6MTU3MTIyNzI1Mw==/detection");
 	$("#link_yt_tutorial").attr("src", "https://www.youtube.com/embed/wogDNmHRzbw?rel=0");
 });
