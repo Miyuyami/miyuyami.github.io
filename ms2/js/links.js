4a03a76901dd1cdcedcced9489c754372250da38
Miyuyami <18267786+Miyuyami@users.noreply.github.com>
2021-12-03 01:57:22 +0200

    [MS2] Added notice about no patches and workaround for Korean MS2

--- ms2/js/links.js
+++ ms2/js/links.js
@@ -1,30 +1,4 @@
-$(document).ready(() => {
-	$("#link_dropbox_cn_en").attr("href", "https://dl.dropboxusercontent.com/s/d9ojnxja5sra867/Xml%20English.zip");
-	$("#link_dropbox_cn_ko").attr("href", "https://dl.dropboxusercontent.com/s/g7nax01y4yjlonf/Xml%20Korean.zip");
-	$("#link_dropbox_cn_ja").attr("href", "ttps://dl.dropboxusercontent.com/s/qyi2fegsx0umjh3/Xml%20Japanese.zip");
-	$("#link_dropbox_cn_en_noname").attr("href", "https://dl.dropboxusercontent.com/s/ws7wgg7lgnmj4vr/Xml%20English%20%28No%20Names%20ver%29.zip");
-	$("#link_dropbox_cn_ko_noname").attr("href", "https://dl.dropboxusercontent.com/s/dqunzarctxzn1lx/Xml%20Korean%20%28No%20Names%20ver%29.zip");
-	$("#link_dropbox_cn_ja_noname").attr("href", "ttps://dl.dropboxusercontent.com/s/v1idr6rou0plldo/Xml%20Japanese%20%28No%20Names%20ver%29.zip");
-	
-	$("#link_dropbox_kr_en").attr("href", "https://dl.dropboxusercontent.com/s/8ja2hwufh2kxrs6/Xml%20English.zip");
-	$("#link_dropbox_kr_zh").attr("href", "https://dl.dropboxusercontent.com/s/lxrdi6k1qo2zx49/Xml%20Chinese.zip");
-	$("#link_dropbox_kr_ja").attr("href", "https://dl.dropboxusercontent.com/s/7jgi8dj6p6ri0pg/Xml%20Japanese.zip");
-	$("#link_dropbox_kr_en_noname").attr("href", "https://dl.dropboxusercontent.com/s/wkzrdibqyx5e2xo/Xml%20English%20%28No%20Names%20ver%29.zip");
-	$("#link_dropbox_kr_zh_noname").attr("href", "https://dl.dropboxusercontent.com/s/4pqv832u18gu27o/Xml%20Chinese%20%28No%20Names%20ver%29.zip");
-	$("#link_dropbox_kr_ja_noname").attr("href", "https://dl.dropboxusercontent.com/s/i32o5e4e1hp3rx1/Xml%20Japanese%20%28No%20Names%20ver%29.zip");
-	
-	$("#link_dropbox_jp_en").attr("href", "https://dl.dropboxusercontent.com/s/h8h2x403x7fghtr/Xml%20English.zip");
-	$("#link_dropbox_jp_zh").attr("href", "https://dl.dropboxusercontent.com/s/yybre0732tgnth4/Xml%20Chinese.zip");
-	$("#link_dropbox_jp_ko").attr("href", "https://dl.dropboxusercontent.com/s/oi8m5sl1s3wn5wl/Xml%20Korean.zip");
-	$("#link_dropbox_jp_en_noname").attr("href", "https://dl.dropboxusercontent.com/s/tusqo9rj6csilzo/Xml%20English%20%28No%20Names%20ver%29.zip");
-	$("#link_dropbox_jp_zh_noname").attr("href", "https://dl.dropboxusercontent.com/s/fciuik7wweqehm9/Xml%20Chinese%20%28No%20Names%20ver%29.zip");
-	$("#link_dropbox_jp_ko_noname").attr("href", "https://dl.dropboxusercontent.com/s/qciv3bmamex5blr/Xml%20Korean%20%28No%20Names%20ver%29.zip");
-	
-	$("#link_dropbox_gb").attr("href", "https://dl.dropboxusercontent.com/s/scepv8sk3dd27er/Xml.zip");
-	$("#link_dropbox_gb_noname").attr("href", "https://dl.dropboxusercontent.com/s/o9j7eexnjk8bawv/Xml%20%28No%20Names%20ver%29.zip");
-	$("#link_dropbox_gb_fontsonly").attr("href", "https://dl.dropboxusercontent.com/s/ykj7xqsv907didw/Xml%20%28Fonts%20only%29.zip");
-	
-	
+$(document).ready(() => {	
 	$("#link_gdrive_cn_en").attr("href", "https://drive.google.com/uc?export=download&id=1WNUB4JBrFGp63r-gh_HTCQAMFEdvD_Dq");
 	$("#link_gdrive_cn_ko").attr("href", "https://drive.google.com/uc?export=download&id=1vWY7vJ4W7zd2pDwmcZLBwEqUkbV1o1Vr");
 	$("#link_gdrive_cn_ja").attr("href", "https://drive.google.com/uc?export=download&id=1JI73xKGLcc60FvdSMLmIvbg5QsKTe5rn");
@@ -45,15 +19,6 @@ $(document).ready(() => {
 	$("#link_gdrive_jp_en_noname").attr("href", "https://drive.google.com/uc?export=download&id=1Ur7VhUCdjOglxty7LO7716spy94b88MS");
 	$("#link_gdrive_jp_zh_noname").attr("href", "https://drive.google.com/uc?export=download&id=1EgbAELQkEtaQu1VrIdyiAF9PI2LQaGEa");
 	$("#link_gdrive_jp_ko_noname").attr("href", "https://drive.google.com/uc?export=download&id=1yiSctlzM-edTk9c57wp62e9j7YHKP0k-");
-	
-	$("#link_gdrive_gb").attr("href", "https://drive.google.com/uc?export=download&id=1tw1_tzCViJkiXsrRepdwBuYK3g-KMJwF");
-	$("#link_gdrive_gb_noname").attr("href", "https://drive.google.com/uc?export=download&id=1yfXf--tfYAzvth9OWhCuEj3yAVTepMNt");
-	$("#link_gdrive_gb_fontsonly").attr("href", "https://drive.google.com/uc?export=download&id=1yNuTQSSSpnyU6h_udILEAwQuWFkXLNO6");
-	
-	
-	$("#link_mega_sound_kr").attr("href", "https://mega.nz/#!KgBgTCaB!6pRJPi0LCAf5VfbJpSdlfHrdijIzHUplEGTb8g0APHQ");
-	$("#link_mega_sound_cn").attr("href", "https://mega.nz/#!bwBGhYzA!-HX-mMe2m4SpqTUsb49qt_L8FEleciPvJxrSXfjAU10");
-	$("#link_mega_sound_jp").attr("href", "https://mega.nz/#!HwAiUajY!zzCqz47I5rdVcIP0usEaWVhJkiXyDun8KGt_a2v5DIw");
 	
 	
 	$("#link_github_ms2tools_releases").attr("href", "https://github.com/Miyuyami/MS2Tools/releases");
